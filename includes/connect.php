a4c1b59e0f154e1c883be97ac4ae2207cb1a3e74
NgocLan12345 <dongoclan2479@gmail.com>
2023-03-03 23:43:42 +0700

    sua data

--- includes/connect.php
+++ includes/connect.php
@@ -6,7 +6,7 @@
     // $db = "btth01_cse485";
     // $por = "3306";
     try{
-        $conn = new PDO('mysql:host=localhost;dbname = btth01_cse485; port=3306', 'root','');
+        $conn = new PDO('mysql:host=localhost;dbname=btth01_cse485;port=3306','root','');
         echo "Connection successfully";
 
     } catch(PDOException $e){
